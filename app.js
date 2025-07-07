3175db198f757fdf9c15d60503883d16a962133b
Ajay-aot <ajay.k@aot-technologies.com>
2025-07-07 22:58:41 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,15 @@
+    const heading = React.createElement(
+        "div",{ 
+               id: "parent"
+        }, [React.createElement(
+        "div",{ 
+               id: "child1"
+        }, "child1"
+    ),React.createElement(
+        "div",{ 
+               id: "child2"
+        }, "child2"
+    )]
+    )
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(heading);
